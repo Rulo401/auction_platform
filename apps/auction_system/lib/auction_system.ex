a9fc957ceefe23f660368d93a45b8d41ad040019
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-05-23 12:19:03 +0200

    Initial app

---
+++ apps/auction_system/lib/auction_system.ex
@@ -0,0 +1,18 @@
+defmodule AuctionSystem do
+  @moduledoc """
+  Documentation for `AuctionSystem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AuctionSystem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
