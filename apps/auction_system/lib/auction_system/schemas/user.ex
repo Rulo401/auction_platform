84df228e2972a978688d5cb516961f68347cd8a4
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-05-28 12:05:51 +0200

    Update schema with changeset

--- apps/auction_system/lib/auction_system/schemas/user.ex
+++ apps/auction_system/lib/auction_system/schemas/user.ex
@@ -6,4 +6,10 @@ defmodule AuctionSystem.Schemas.User do
     field :balance, :float, default: 0.0
     field :freezed, :float, default: 0.0
   end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> Ecto.Changeset.cast(params, [:balance, :freezed])
+    |> Ecto.Changeset.validate_required([:nickname,:balance, :freezed])
+  end
 end
