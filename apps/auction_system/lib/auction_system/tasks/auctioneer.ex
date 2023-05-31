ecd8c48fd227194bdaa9146c24d102b7f8c87171
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-05-31 19:00:20 +0200

    Upload Auctioneer: module in charge of updating the bids

---
+++ apps/auction_system/lib/auction_system/tasks/auctioneer.ex
@@ -0,0 +1,110 @@
+defmodule AuctionSystem.Tasks.Auctioneer do
+  alias AuctionSystem.Schemas.{User,Auction}
+  alias AuctionSystem.Repo
+  import Ecto.Query
+
+  def bid(client_pid, manager_pid, _, _) when not is_pid(client_pid) or not is_pid(manager_pid) do
+    {:error, "Invalid client or manager pid"}
+  end
+
+  def bid(client_pid, manager_pid, call_id, {user_id, auction_id, bid}) do
+    response = process_bid({user_id, auction_id, bid})
+    send(manager_pid, {:free, auction_id})
+    send(client_pid,{call_id, response})
+  end
+
+  defp process_bid({user_id, auction_id, _}) when not is_integer(user_id) or not is_integer(auction_id) do
+    {:error, "User_id and auction_id must be integers"}
+  end
+
+  defp process_bid({_, _, bid}) when not is_float(bid) or bid <= 0 do
+    {:error, "Bid must be a positive float"}
+  end
+
+  defp process_bid({user_id, auction_id, bid}) do
+    case Repo.transaction(fn -> bid_transaccion({user_id, auction_id, bid}) end) do
+      {:error, :not_enough_balance} ->
+        {:error, "User balance is not enough to bid the amount"}
+      {:error, :auction_not_found} ->
+        {:error, "Auction is closed or does not exist"}
+      {:error, :lower_than_current_bid} ->
+        {:error, "Bid must be higher than the current one"}
+      {:error, :update_error} ->
+        {:error, "Internal error. Try it later"}
+      {:ok, response} ->
+        response
+    end
+  end
+
+  defp bid_transaccion({user_id, auction_id, bid}) do
+    case User |> Repo.get(user_id) do
+      user when user.balance >= bid ->
+        case Repo.one(from a in Auction, where: a.id == ^auction_id and a.end > datetime_add(^NaiveDateTime.utc_now(),0,"second")) do
+          nil ->
+            Repo.rollback(:auction_not_found)
+          auc when auc.bid < bid ->
+            #prev_bidder = auc.bidder |> User.changeset(%{balance: auc.bidder.balance + auc.bid, freezed: auc.bidder.freezed - auc.bid})
+            prev_bidder = User |> Repo.get(auc.bidder_id)
+            case prev_bidder do
+              nil ->
+                Repo.rollback(:update_error)
+              _ ->
+                nil
+            end
+            prev_bidder = prev_bidder |> User.changeset(%{balance: prev_bidder.balance + auc.bid, freezed: prev_bidder.freezed - auc.bid})
+            case Repo.update(prev_bidder) do
+              {:error, _} ->
+                Repo.rollback(:update_error)
+              _ ->
+                nil
+            end
+            auc = auc |> Auction.changeset(%{bidder_id: user.id, bid: bid, end: increment_end_date(auc.end, 5)})
+            case Repo.update(auc) do
+              {:error, _} ->
+                Repo.rollback(:update_error)
+              _ ->
+                nil
+            end
+            user = user |> User.changeset(%{balance: user.balance - bid, freezed: user.freezed + bid})
+            case Repo.update(user) do
+              {:ok, user} ->
+                {:ok, user.balance}
+              {:error, _} ->
+                Repo.rollback(:update_error)
+            end
+          auc when is_nil(auc.bid) and auc.minBid < bid ->
+            auc = auc |> Auction.changeset(%{bidder_id: user.id, bid: bid, end: increment_end_date(auc.end, 5)})
+            case Repo.update(auc) do
+              {:error, _} ->
+                Repo.rollback(:update_error)
+              _ ->
+                nil
+            end
+            user = user |> User.changeset(%{balance: user.balance - bid, freezed: user.freezed + bid})
+            case Repo.update(user) do
+              {:ok, user} ->
+                {:ok, user.balance}
+              {:error, _} ->
+                Repo.rollback(:update_error)
+            end
+          _ ->
+            Repo.rollback(:lower_than_current_bid)
+        end
+      _ ->
+        Repo.rollback(:not_enough_balance)
+    end
+
+  end
+
+  defp increment_end_date(current_time, minutes) do
+    proposed_time = NaiveDateTime.utc_now()
+      |> NaiveDateTime.add(minutes, :minute)
+      |> NaiveDateTime.truncate(:second)
+    case NaiveDateTime.compare(current_time, proposed_time) do
+      :lt ->
+        proposed_time
+      _ ->
+        current_time
+    end
+  end
+end
---
+++ apps/auction_system/test/tasks/auctioneer_test.exs
@@ -0,0 +1,277 @@
+defmodule AuctionSystemTest.Tasks.AuctioneerTest do
+  use AuctionSystem.RepoCase
+  alias AuctionSystem.Tasks.Auctioneer
+  alias AuctionSystem.Schemas.{User,Auction}
+  doctest Auctioneer
+
+  describe "Testing wrong calls" do
+    test "Invalid pids" do
+      pid = self()
+
+      assert {:error, "Invalid client or manager pid"} == Auctioneer.bid(:wrong_pid, pid, :pet, nil)
+      assert {:error, "Invalid client or manager pid"} == Auctioneer.bid(pid, :wrong_pid, :pet, nil)
+      assert {:error, "Invalid client or manager pid"} == Auctioneer.bid(:wrong_pid, :wrong_pid, :pet, nil)
+    end
+
+    test "Invalid ids" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {"0", 1, 10.0}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 1
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "User_id and auction_id must be integers"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {0, "1", 10.0}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == "1"
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "User_id and auction_id must be integers"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+
+    test "Invalid bids" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {0, 1, "10.0"}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 1
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Bid must be a positive float"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {0, 1, -1.2}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 1
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Bid must be a positive float"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {0, 2, 0.0}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 2
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Bid must be a positive float"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+  end
+
+  describe "Valid calls" do
+    setup do
+      trunc = "TRUNCATE TABLE CATEGORIES,
+      WEAPONS,
+      SKINS,
+      ITEMS,
+      USERS,
+      AUCTIONS;"
+
+      ins_cat = "INSERT INTO CATEGORIES
+    VALUES (0, 'CategoryTest');"
+      ins_wea ="INSERT INTO WEAPONS
+    VALUES (0, 'WeaponTest', 0);"
+      ins_ski ="INSERT INTO SKINS
+    VALUES (0, 0, 453, 'Skin1', 0.1, 0.4),
+                  (1, 0, 58, 'Skin2', 0.01, 0.7);"
+      ins_ite = "INSERT INTO ITEMS
+    VALUES (0, 0, 30, 0.2),
+                  (1, 0, 67, 0.399),
+                  (2, 1, 11, 0.65);"
+      ins_use = "INSERT INTO USERS
+    VALUES (0, 'UserTest1', 0.0, 0.0),
+                  (1, 'UserTest2', 1.0, 0.0),
+                  (2, 'UserTest3', 49.6, 0.5);"
+      ins_auc = "INSERT INTO AUCTIONS
+    VALUES (0, 0, 0, 1, 25.0, 27.2, '2023-05-29', TRUE),
+                  (1, 0, 1, NULL, 50.0, NULL, '2024-05-29', FALSE),
+                  (2, 0, 2, 2, 0.2, 0.5, '2024-05-29', FALSE);"
+
+      {:ok, _} = Repo.query(trunc)
+      {:ok, _} = Repo.query(ins_cat)
+      {:ok, _} = Repo.query(ins_wea)
+      {:ok, _} = Repo.query(ins_ski)
+      {:ok, _} = Repo.query(ins_ite)
+      {:ok, _} = Repo.query(ins_use)
+      {:ok, _} = Repo.query(ins_auc)
+
+      :ok
+    end
+
+    test "Pujar una cantidad mayor al balance" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {1, 2, 1.1}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 2
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "User balance is not enough to bid the amount"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+
+    test "Pujar en una subasta cerrada" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {2, 0, 10.0}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 0
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Auction is closed or does not exist"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+
+    test "Pujar una cantidad menor a la puja minima" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {1, 1, 0.4}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 1
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Bid must be higher than the current one"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+
+    test "Pujar una cantidad menor a la puja actual" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {1, 2, 0.4}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 2
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:error, "Bid must be higher than the current one"}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+    end
+
+    test "Pujar correctamente" do
+      pid = self()
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {1, 2, 0.6}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 2
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:ok, 0.4}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+
+      auc2 = Auction |> Repo.get(2) |> Repo.preload(:bidder)
+
+      assert auc2.seller_id == 0
+      assert auc2.item_id == 2
+      assert auc2.bidder_id == 1
+      assert auc2.bid == 0.6
+      refute auc2.paid
+
+      assert auc2.bidder.balance == 0.4
+      assert auc2.bidder.freezed == 0.6
+
+      user2 = User |> Repo.get(2)
+
+      assert user2.balance == 50.1
+      assert user2.freezed == 0.0
+
+      spawn(fn -> Auctioneer.bid(pid, pid, :test, {2, 1, 50.1}) end)
+
+      receive do
+        {:free, id} ->
+          assert id == 1
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+      receive do
+        {:test, response} ->
+          assert response == {:ok, 0.0}
+        after 5000 ->
+          refute "TIMEOUT" == "TIMEOUT"
+      end
+
+      auc1 = Auction |> Repo.get(1) |> Repo.preload(:bidder)
+
+      assert auc1.seller_id == 0
+      assert auc1.item_id == 1
+      assert auc1.bidder_id == 2
+      assert auc1.bid == 50.1
+      refute auc1.paid
+
+      assert auc1.bidder.balance == 0.0
+      assert auc1.bidder.freezed == 50.1
+    end
+  end
+end
