e49c734bc328200e6cc9aff7a6888c3959fe87a1
Javier Sabín <javisabin@gmail.com>
2023-06-01 00:55:03 +0200

    Fifo module and tests upload

--- apps/fifo/lib/fifo.ex
+++ apps/fifo/lib/fifo.ex
@@ -1,3 +1,74 @@
 defmodule Fifo do
-  nil
+  use GenServer
+
+  def init(_) do
+    {:ok, []}
+  end
+
+  def handle_cast({:push, element}, state) do
+    {:noreply, state ++ [element]}
+  end
+
+  def handle_call(:pull, _from, []) do
+    {:reply, nil, []}
+  end
+
+  def handle_call(:pull, _from, [element|tail]) do
+    {:reply, {:ok, element}, tail}
+  end
+
+  def handle_call({:pull_backfilling, _}, _from, []) do
+    {:reply, {:ok, nil}, []}
+  end
+
+  def handle_call({:pull_backfilling, fun}, _from, state) do
+    {new_state, response} = pull_backfilling([], state, fun)
+    {:reply, response, new_state}
+  end
+
+  def handle_call(:seek, _from, []) do
+    {:reply, {:ok, nil}, []}
+  end
+
+  def handle_call(:seek, _from, [head | tail]) do
+    {:reply, {:ok, head}, [head | tail]}
+  end
+
+
+  def handle_call({:seek_backfilling, _fun}, _from, []) do
+    {:reply, {:ok, nil}, []}
+  end
+
+  def handle_call({:seek_backfilling, fun}, _from, state) do
+    response = seek_backfilling(state, fun)
+    {:reply, response, state}
+  end
+
+
+  defp pull_backfilling(prefix, [], _fun) do
+    {prefix, {:ok, nil}}
+  end
+
+  defp pull_backfilling(prefix, [head | tail], fun) do
+    cond do
+      fun.(head)->
+        {prefix ++ tail, {:ok, head}}
+      true ->
+        pull_backfilling(prefix ++ [head], tail, fun)
+
+    end
+  end
+
+  defp seek_backfilling([], _fun) do
+    {:ok, nil}
+  end
+
+  defp seek_backfilling([head | tail], fun) do
+    cond do
+      fun.(head)->
+        {:ok, head}
+      true ->
+        seek_backfilling(tail, fun)
+    end
+  end
 end
--- apps/fifo/test/fifo_test.exs
+++ apps/fifo/test/fifo_test.exs
@@ -2,4 +2,67 @@ defmodule FifoTest do
   use ExUnit.Case
   alias Fifo
   doctest Fifo
+
+  setup_all do
+    {_, pid} = GenServer.start_link(Fifo, [])
+    Process.register(pid, FifoServer)
+    :ok
+  end
+
+  describe "Empty state Tests" do
+    # Pull with empty FIFO
+    test "Pull with Empty State" do
+      assert nil == GenServer.call(FifoServer, :pull, 500)
+    end
+
+    # Pull_backfilling with empty FIFO
+    test "Pull_backfilling with Empty State" do
+      assert {:ok, nil} == GenServer.call(FifoServer, {:pull_backfilling, fn _x -> false end}, 500)
+    end
+
+
+    # Seek with empty FIFO
+    test "Seek with Empty State" do
+      assert {:ok, nil} == GenServer.call(FifoServer, :seek, 500)
+    end
+
+    # Seek_backfilling with empty FIFO
+    test "Seek_backfilling with Empty State" do
+      assert {:ok, nil} == GenServer.call(FifoServer, {:seek_backfilling, fn _x -> false end}, 500)
+    end
+  end
+
+  describe "Tests with state" do
+    setup do
+      {_, pid} = GenServer.start_link(Fifo, [])
+      #Process.register(pid, FifoServer)
+      {:ok, server: pid}
+    end
+
+    # Pull Push and Seek with elements
+    test "Push_Pull_Seek" , state do
+      GenServer.cast(state.server, {:push, 3})
+      assert {:ok, 3} == GenServer.call(state.server, :pull, 500)
+
+      GenServer.cast(state.server, {:push, 4})
+      GenServer.cast(state.server, {:push, 5})
+      assert {:ok, 4} == GenServer.call(state.server, :pull, 500)
+      assert {:ok, 5} == GenServer.call(state.server, :seek, 500)
+      assert {:ok, 5} == GenServer.call(state.server, :pull, 500)
+    end
+
+    # Pull and Seek backfilling with elements
+    test "Pull_Seek_backfilling" , state do
+      GenServer.cast(state.server, {:push, 1})
+      GenServer.cast(state.server, {:push, 2})
+      GenServer.cast(state.server, {:push, 3})
+      GenServer.cast(state.server, {:push, 4})
+
+      assert {:ok, 2} == GenServer.call(state.server, {:pull_backfilling, fn x -> x > 1 end}, 500)
+
+      assert {:ok, 3} == GenServer.call(state.server, {:seek_backfilling, fn x -> x > 1 end}, 500)
+      assert {:ok, 1} == GenServer.call(state.server, :pull, 500)
+      assert {:ok, 3} == GenServer.call(state.server, :pull, 500)
+    end
+  end
 end
