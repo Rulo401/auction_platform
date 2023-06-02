539c473f1dc61b5983855e7f9ed7a800aeb64891
Javier Sabín <javisabin@gmail.com>
2023-06-02 21:46:24 +0200

    Credit_supervisor documentation added

--- apps/auction_system/lib/auction_system/supervisors/credit_supervisor.ex
+++ apps/auction_system/lib/auction_system/supervisors/credit_supervisor.ex
@@ -2,11 +2,13 @@ defmodule AuctionSystem.Supervisors.CreditSupervisor do
   use Supervisor
   alias AuctionSystem.Servers.CreditServer
 
+  @spec start_link(any) :: Supervisor.on_start()
   def start_link(_) do
     Supervisor.start_link(__MODULE__, name: __MODULE__)
   end
 
   @impl true
+  @spec init(any) :: {:ok, {Supervisor.sup_flags(), [Supervisor.child_spec()]}}
   def init(_) do
     children = [
       {DynamicSupervisor, name: :balance_ds, strategy: :one_for_one, max_children: 250},
