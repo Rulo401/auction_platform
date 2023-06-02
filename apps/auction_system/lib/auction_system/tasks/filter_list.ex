ef58892f2489429442e39ac64f91ca30473d5ad5
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-06-02 21:10:33 +0200

    Added documentation to FilterList

--- apps/auction_system/lib/auction_system/tasks/filter_list.ex
+++ apps/auction_system/lib/auction_system/tasks/filter_list.ex
@@ -3,6 +3,7 @@ defmodule AuctionSystem.Tasks.FilterList do
   alias AuctionSystem.Repo
   import Ecto.Query
 
+  @spec list_categories(from :: pid | GenServer.from()) :: :ok
   def list_categories(from) do
     query = from c in Category, select: {c.id, c.name}, order_by: [asc: c.id]
     stream = Repo.stream(query)
@@ -18,6 +19,7 @@ defmodule AuctionSystem.Tasks.FilterList do
     answer(from, response)
   end
 
+  @spec list_weapons(from :: pid | GenServer.from(), category_id :: integer) :: :ok
   def list_weapons(from,category_id) do
     query = from w in Weapon, where: w.category_id == ^category_id, select: {w.id, w.name}, order_by: [asc: w.id]
     stream = Repo.stream(query)
@@ -33,6 +35,7 @@ defmodule AuctionSystem.Tasks.FilterList do
     answer(from, response)
   end
 
+  @spec list_skins(from :: pid | GenServer.from(), weapon_id :: integer) :: :ok
   def list_skins(from, weapon_id)do
     query = from s in Skin, where: s.weapon_id == ^weapon_id, select: {s.id, s.name}, order_by: [asc: s.id]
     stream = Repo.stream(query)
@@ -50,6 +53,7 @@ defmodule AuctionSystem.Tasks.FilterList do
 
   defp answer(from, response) when is_pid(from) do
     send(from, {:test, response})
+    :ok
   end
 
   defp answer(from, response) do
