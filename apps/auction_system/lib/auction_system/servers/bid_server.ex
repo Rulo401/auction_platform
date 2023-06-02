434731ed74a429d46f8c561f53d718ee5debaf2c
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-06-02 22:01:08 +0200

    Added documentation to MarketServer

--- apps/auction_system/lib/auction_system/servers/bid_server.ex
+++ apps/auction_system/lib/auction_system/servers/bid_server.ex
@@ -1,16 +1,20 @@
 defmodule AuctionSystem.Servers.BidServer do
   use GenServer
+  @type bid() :: {user_id :: integer, auction_id :: integer, bid :: float}
 
+  @spec start_link([Supervisor.supervisor()]) :: {:ok, pid()}
   def start_link([supervisor]) do
     GenServer.start_link(__MODULE__, supervisor, name: __MODULE__)
   end
 
   @impl true
+  @spec init(Supervisor.supervisor()) :: {:ok, Supervisor.supervisor()}
   def init(supervisor) do
     {:ok, supervisor}
   end
 
   @impl true
+  @spec handle_call(bid(), from :: pid | GenServer.from(), Supervisor.supervisor()) :: {:noreply, Supervisor.supervisor()}
   def handle_call({user_id, auction_id, bid}, from, supervisor) do
     list = Supervisor.which_children(supervisor)
     fifo = Enum.find_value(list, fn x -> find_fifo(x) end)
