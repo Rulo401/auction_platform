861a9fa24a6291397809e9edad5e73ec5afc445f
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-06-02 21:12:04 +0200

    Added documentation to Balance

--- apps/auction_system/lib/auction_system/tasks/balance.ex
+++ apps/auction_system/lib/auction_system/tasks/balance.ex
@@ -2,6 +2,7 @@ defmodule AuctionSystem.Tasks.Balance do
   alias AuctionSystem.Repo
   alias AuctionSystem.Schemas.User
 
+  @spec balance(from :: GenServer.from(), user_id :: integer) :: :ok
   def balance(from, user_id) do
     response = case User |> Repo.get(user_id) do
       nil ->
