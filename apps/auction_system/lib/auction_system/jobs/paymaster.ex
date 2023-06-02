291b14a4cfdd4c870e326240ebdfbc46663c6bd6
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2023-06-02 17:50:09 +0200

    Improve PayMaster access to database

--- apps/auction_system/lib/auction_system/jobs/paymaster.ex
+++ apps/auction_system/lib/auction_system/jobs/paymaster.ex
@@ -28,26 +28,38 @@ defmodule AuctionSystem.Jobs.Paymaster do
   end
 
   def pay_now() do
-    case Repo.transaction(fn() -> pay_transaction() end) do
-      {:ok, _} ->
-        {:ok, nil}
-      {:error, _} ->
-        {:error, "Database error"}
-    end
+    pay_now(:ok, 0)
+   #case Repo.transaction(fn() -> pay_transaction() end) do
+   #  {:ok, _} ->
+   #    :ok
+   #  {:error, _} ->
+   #    {:error, "Database error"}
+   # end
+  end
+
+  defp pay_now({:error, _}, 0) do
+    {:error, "Database error"}
+  end
+
+  defp pay_now({:ok, nil}, n) do
+    {:ok, "#{inspect(n)} changes has been done"}
+  end
+
+  defp pay_now(_, n) do
+    pay_now(Repo.transaction(fn() -> pay_transaction() end), n + 1)
   end
 
   defp pay_transaction() do
-    query = from au in Auction, where: not is_nil(au.bid) and not au.paid and au.end < datetime_add(^NaiveDateTime.utc_now(),0,"second"), preload: :seller, preload: :bidder
-    pay_operation(Repo.all(query))
+    query = from au in Auction, where: not is_nil(au.bid) and not au.paid and au.end < datetime_add(^NaiveDateTime.utc_now(),0,"second"), limit: 1, preload: :seller, preload: :bidder
+    pay_operation(Repo.one(query))
   end
 
-  defp pay_operation([]) do  end
+  defp pay_operation(nil) do nil end
 
-  defp pay_operation([auc | rest]) do
+  defp pay_operation(auc) do
     Repo.update(Ecto.Changeset.change(auc.seller, balance: auc.seller.balance + auc.bid))
     Repo.update(Ecto.Changeset.change(auc.bidder, freezed: auc.bidder.freezed - auc.bid))
     Repo.update(Ecto.Changeset.change(auc, paid: true))
-    pay_operation(rest)
   end
 
 end
