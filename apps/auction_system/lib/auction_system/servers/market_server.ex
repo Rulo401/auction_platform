0244e8baa1e72cf18986dd87dd2f4418e2fb5179
Javier Sabín <javisabin@gmail.com>
2023-06-02 16:47:29 +0200

    Market_server bug on redirect message corrected

--- apps/auction_system/lib/auction_system/servers/market_server.ex
+++ apps/auction_system/lib/auction_system/servers/market_server.ex
@@ -13,56 +13,42 @@ defmodule AuctionSystem.Servers.MarketServer do
 
   @impl true
   def handle_call(:list_category, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_categories, [pid, from]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_categories, [from]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:list_weapons, category_id}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_weapons, [pid, from, category_id]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_weapons, [from, category_id]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:list_skins, weapon_id}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_skins, [pid, from, weapon_id]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: FilterList, start: {FilterList, :list_skins, [from, weapon_id]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:list_auctions,:all}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionList, start: {AuctionList, :list_auctions, [pid, from, :all]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionList, start: {AuctionList, :list_auctions, [from, :all]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:list_auctions, method, arg}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionList, start: {AuctionList, :list_auctions, [pid, from, method, arg]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionList, start: {AuctionList, :list_auctions, [from, method, arg]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:auction_item, user_id, item_def, days, minBid}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :auction_item, [pid, from, user_id, item_def, days, minBid]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :auction_item, [from, user_id, item_def, days, minBid]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:auction_item, user_id, item_def, days}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :auction_item, [pid, from, user_id, item_def, days]}})
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :auction_item, [from, user_id, item_def, days]}})
     {:noreply, supervisor}
   end
 
   def handle_call({:auction_data, auction_id}, from, supervisor) do
-    pid = self()
-    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :get_auction_data, [pid, from, auction_id]}})
-    {:noreply, supervisor}
-  end
-
-  @impl true
-  def handle_info({:market, from, response}, supervisor) do
-    GenServer.reply(from, response)
+    DynamicSupervisor.start_child(find_ds(supervisor), %{ id: AuctionManager, start: {AuctionManager, :get_auction_data, [from, auction_id]}})
     {:noreply, supervisor}
   end
 
