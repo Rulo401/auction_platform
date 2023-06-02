ec0266de997f4bf095f581097145aa43d1ce9d14
Javier Sabín <javisabin@gmail.com>
2023-06-02 18:54:27 +0200

    Client_api and test upload

--- apps/client_api/lib/client_api.ex
+++ apps/client_api/lib/client_api.ex
@@ -1,3 +1,91 @@
 defmodule ClientApi do
-  nil
+  alias AuctionSystem.Servers.UserServer
+  alias AuctionSystem.Servers.CreditServer
+  alias AuctionSystem.Servers.MarketServer
+  alias AuctionSystem.Servers.BidServer
+  @type user_id() :: integer
+  @type auction_id() :: integer
+  @type category_id() :: integer
+  @type category_name() :: String.t()
+  @type weapon_id() :: integer
+  @type weapon_name() :: String.t()
+  @type skin_id() :: integer
+  @type skin_name() :: String.t()
+  @type balance() :: float
+  @type item() ::  %{skin_id: skin_id(), seed: integer, sfloat: float}
+  @type auction() :: %{weapon: String.t(), skin: String.t(), seed: integer, skinFloat: float, bid: float, end: NaiveDateTime.t()}
+
+  #UserServer calls
+  @spec create_user(username :: String.t()) :: {:ok, user_id()}
+  def create_user(username) do
+    GenServer.call(UserServer, {:create, username})
+  end
+
+  @spec delete_user(username :: String.t()) :: :ok
+  def delete_user(username) do
+    GenServer.call(UserServer, {:delete, username})
+  end
+
+  @spec login_user(username :: String.t()) :: {:ok, user_id()}
+  def login_user(username) do
+    GenServer.call(UserServer, {:login, username})
+  end
+
+  #CreditServer calls
+  @spec deposit(user_id :: user_id(), amount :: float) :: {:ok, balance()}
+  def deposit(user_id, amount) do
+    GenServer.call(CreditServer, {:deposit, user_id, amount})
+  end
+
+  @spec withdraw(user_id :: user_id(), amount :: float) :: {:ok, balance()}
+  def withdraw(user_id, amount) do
+    GenServer.call(CreditServer, {:withdraw, user_id, amount})
+  end
+
+  @spec check_balance(user_id :: user_id()) :: {:ok, balance()}
+  def check_balance(user_id) do
+    GenServer.call(CreditServer, {:balance, user_id})
+  end
+
+  #BidServer calls
+  @spec bid(user_id :: user_id(), auction_id :: auction_id(), amount :: float) :: {:ok, balance()}
+  def bid(user_id, auction_id, amount) do
+    GenServer.call(BidServer, {user_id, auction_id, amount})
+  end
+
+  #MarketServer calls
+  @spec list_auctions() :: {:ok, list(auction_id())}
+  def list_auctions() do
+    GenServer.call(MarketServer, {:list_auctions, :all})
+  end
+
+  @spec list_auctions(method :: :category | :weapon | :skin, id :: category_id() | weapon_id() | skin_id()) :: {:ok, list(auction_id())}
+  def list_auctions(method, id) do
+    GenServer.call(MarketServer, {:list_auctions, method, id})
+  end
+
+  @spec list_categories() :: {:ok, list(category_id())}
+  def list_categories() do
+    GenServer.call(MarketServer, :list_category)
+  end
+
+  @spec list_weapons(category_id :: category_id()) :: {:ok, list({weapon_id(), weapon_name()})}
+  def list_weapons(category_id) do
+    GenServer.call(MarketServer, {:list_weapons, category_id})
+  end
+
+  @spec list_skins(weapon_id :: weapon_id()) :: {:ok, list({skin_id(), skin_name()})}
+  def list_skins(weapon_id) do
+    GenServer.call(MarketServer, {:list_skins, weapon_id})
+  end
+
+  @spec create_auction(user_id :: user_id(), item_def :: item(), days :: integer, minBid :: float) :: {:ok, auction_id :: auction_id()}
+  def create_auction(user_id, item_def, days, minBid) do
+    GenServer.call(MarketServer, {:auction_item, user_id, item_def, days, minBid})
+  end
+
+  @spec auction_data(auction_id :: auction_id()) :: {:ok, auction()}
+  def auction_data(auction_id) do
+    GenServer.call(MarketServer, {:auction_data, auction_id})
+  end
 end
--- apps/client_api/test/client_api_test.exs
+++ apps/client_api/test/client_api_test.exs
@@ -1,3 +1,148 @@
 defmodule ClientApiTest do
-  nil
+  use AuctionSystem.RepoCase
+  alias AuctionSystem.Repo
+  alias ClientApi
+  alias AuctionSystem.Supervisors.SystemSupervisor
+  doctest ClientApi
+
+  setup do
+    trunc = "TRUNCATE TABLE CATEGORIES,
+    WEAPONS,
+    SKINS,
+    ITEMS,
+    USERS,
+    AUCTIONS;"
+
+    ins_cat = "INSERT INTO CATEGORIES
+VALUES (0, 'Knives'),
+              (1, 'Gloves'),
+              (2, 'Rifles'),
+              (3, 'Pistols');"
+    ins_wea = "INSERT INTO WEAPONS
+  VALUES (0, 'Ak-47', 2),
+                              (1, 'M4A4', 2),
+                              (2, 'AWP', 2),
+                              (3, 'Karambit', 0);"
+    ins_ski ="INSERT INTO SKINS
+VALUES (0, 0, 453, 'Vulcan', 0.1, 0.4),
+              (1, 0, 58, 'Redline', 0.01, 0.7);"
+    ins_ite = "INSERT INTO ITEMS
+  VALUES (0, 0, 30, 0.2),
+                (1, 0, 67, 0.399),
+                (2, 1, 11, 0.65);"
+    ins_use = "INSERT INTO USERS
+  VALUES (0, 'UserTest1', 0.0, 0.0),
+                (1, 'UserTest2', 0.0, 27.2),
+                (2, 'UserTest3', 50.5, 0.5);"
+    ins_auc = "INSERT INTO AUCTIONS
+  VALUES (0, 0, 0, 1, 25.0, 27.2, '2024-05-29', FALSE),
+                (1, 0, 1, NULL, 50.0, NULL, '2023-05-29', FALSE),
+                (2, 0, 2, 2, 0.2, 0.5, '2024-05-29', FALSE);"
+
+      #Insert data into the DB
+      {:ok, _} = Repo.query(trunc)
+      {:ok, _} = Repo.query(ins_cat)
+      {:ok, _} = Repo.query(ins_wea)
+      {:ok, _} = Repo.query(ins_ski)
+      {:ok, _} = Repo.query(ins_ite)
+      {:ok, _} = Repo.query(ins_use)
+      {:ok, _} = Repo.query(ins_auc)
+
+      SystemSupervisor.start_link([])
+      :ok
+
+
+  end
+
+
+  test "UserServer " do
+    #Create a user, delete it and try to login
+    {status, _} =  ClientApi.create_user("UserTest4")
+    assert status == :ok
+    assert ClientApi.delete_user("UserTest4") == :ok
+    assert ClientApi.delete_user("UserTest4") == {:error, "Username not found"}
+    {status, _} = ClientApi.login_user("UserTest1")
+    assert status == :ok
+    assert ClientApi.login_user("UserTest4") == {:error, "Username not found"}
+    assert ClientApi.create_user("UserTest2") == {:error, "Username already exists"}
+  end
+
+  test "CreditServer" do
+    #Deposit,withdraw and check balance from a user
+    {status, _} = ClientApi.login_user("UserTest1")
+    assert status == :ok
+    {status, _} = ClientApi.login_user("UserTest3")
+    assert status == :ok
+    assert ClientApi.deposit(0, 10) == {:ok, 10}
+    assert ClientApi.deposit(0, -5) == {:error, "Deposit amount must be a positive number"}
+    assert ClientApi.withdraw(2, 10) == {:ok, 40.5}
+    assert ClientApi.check_balance(0) == {:ok, 10}
+    assert ClientApi.check_balance(2) == {:ok, 40.5}
+  end
+
+  test "BidServer" do
+    #Bid on an auction
+    {status, id1} = ClientApi.login_user("UserTest1")
+    assert status == :ok
+    assert ClientApi.deposit(0, 30) == {:ok, 30.0}
+    {status, id3} = ClientApi.login_user("UserTest3")
+    assert status == :ok
+    assert ClientApi.bid(id1, 2, 1.0) == {:ok,29.0}
+    assert ClientApi.bid(id3, 2, 1.5) == {:ok, 49.0}
+    assert ClientApi.bid(id1, 2, 2.0) == {:ok, 27.0}
+    assert ClientApi.bid(id3, 2, 2.5) == {:ok, 46.5}
+    assert ClientApi.bid(id1, 2, 3.0) == {:ok, 24.0}
+
+    #Bid on a closed auction
+    assert ClientApi.bid(id3, 1, 30.0) == {:error, "Auction is closed or does not exist"}
+
+    #Bid with a non existant auction
+    assert ClientApi.bid(id1, 4, 30.0) == {:error, "Auction is closed or does not exist"}
+
+    #Bid with an amount lower than the current bid
+    assert ClientApi.bid(id1, 2, 0.8) == {:error, "Bid must be higher than the current one"}
+
+    #Bid with an integer amount
+    assert ClientApi.bid(id1, 2, 1) == {:error, "Bid must be a positive float"}
+  end
+
+  #Make tests for the Client api of MarketServer
+  test "MarketServer" do
+    #List all auctions
+    assert ClientApi.list_auctions() == {:ok, [0, 2]}
+
+    #List all auctions of a skin
+    assert ClientApi.list_auctions(:skin, 1) == {:ok, [2]}
+    assert ClientApi.list_auctions(:skin, 0) == {:ok, [0]}
+    assert ClientApi.list_auctions(:skin, 2) == {:error, "Invalid skin or No auctions listed for that skin"}
+
+    #List all auctions of a weapon
+    assert ClientApi.list_auctions(:weapon, 0) == {:ok, [0,2]}
+    assert ClientApi.list_auctions(:weapon, 1) == {:error, "Invalid weapon or No auctions listed for that weapon"}
+
+    #List all auctions of a category
+    assert ClientApi.list_auctions(:category, 2) == {:ok, [0,2]}
+    assert ClientApi.list_auctions(:category, 0) == {:error, "Invalid category or No auctions listed for that category"}
+
+    #List all categories
+    assert ClientApi.list_categories() == {:ok, [{0, "Knives"}, {1, "Gloves"}, {2, "Rifles"}, {3, "Pistols"}]}
+
+    #List all skins of a weapon
+    assert ClientApi.list_skins(0) == {:ok, [{0, "Vulcan"},{1, "Redline"}]}
+
+    #List all weapons of a category
+    assert ClientApi.list_weapons(0) == {:ok, [{3, "Karambit"}]}
+
+    #Create an auction
+    {status, id} = ClientApi.login_user("UserTest1")
+    assert status == :ok
+    item = %{skin_id: 0, seed: 233, sfloat: 0.33}
+    {status, _} = ClientApi.create_auction(id, item, 10, 5.0)
+    assert status == :ok
+
+    #Get auction data
+    {_,date} =  NaiveDateTime.new(2024,05,29,00,00,00)
+    auction_data = %{weapon: "Ak-47", skin: "Vulcan", seed: 30, skinFloat: 0.2, bid: 27.2, end: date}
+    assert ClientApi.auction_data(0) == {:ok, auction_data}
+  end
 end
